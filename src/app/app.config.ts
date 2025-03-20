7f8cff23d08e62a17329bca1cd03c26e3c3f2295
Javier Rojas <javierra@unicauca.edu.co>
2025-03-20 02:08:19 -0500

    feat: Agregar componente de lista de reproducción y servicio asociado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    MessageService,
+    CommonModule,
+    BrowserAnimationsModule,
+    provideAnimations(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { PlaylistListComponent } from './components/playlist-list/playlist-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-     { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  {
+    path: 'playlists',
+    component: PlaylistListComponent,
+    canActivate: [AuthGuard],
+  },
 ];
