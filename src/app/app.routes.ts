c583e44d7658f9b4c8a40c9d7450b7b6ad218501
Javier Rojas <javierra@unicauca.edu.co>
2025-03-20 02:16:18 -0500

    feat: Agregar guardia de no autenticación y actualizar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PlaylistListComponent } from './components/playlist-list/playlist-list.component';
 import { AuthGuard } from './guards/auth.guard';
+import { NoAuthGuard } from './guards/no-auth.guard';
 import { PlaylistDetailComponent } from './components/playlist-detail/playlist-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  {
+    path: '',
+    component: LoginComponent,
+    canActivate: [NoAuthGuard],
+  },
   {
     path: 'playlists',
     component: PlaylistListComponent,
